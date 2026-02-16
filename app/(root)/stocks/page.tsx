fe973ff3f5c57853577a33597c51d100de90d7f6
sigma <jakecoding44@gmail.com>
2026-02-16 10:51:16 +0000

    fix: remove duplicate route group

--- app/(root)/stocks/page.tsx
+++
@@ -1,9 +0,0 @@
-import { searchStocks } from "@/lib/actions/finnhub.actions";
-import StocksClient from "./stocks.client";
-
-export default async function StocksPage() {
-  // preload popular stocks for search
-  const initialStocks = await searchStocks();
-
-  return <StocksClient initialStocks={initialStocks} />;
-}
